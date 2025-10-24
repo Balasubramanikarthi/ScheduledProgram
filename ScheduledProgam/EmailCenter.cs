ae17343fedf22db2db645ee7a24e695e5e788e7c
KarthiBalasubramani <karthikeyan.selvisubramani@gmail.com>
2025-10-24 14:54:06 +0530

    EmailCenter New Entity Created

---
+++ ScheduledProgam/EmailCenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduledProgram
+{
+    class EmailCenter
+    { 
+        public string FromAddress { get; set; }
+        public string ToAddress   { get; set; }
+        public string Subject     { get; set; }
+        public string Content     { get; set; }
+    }
+}
